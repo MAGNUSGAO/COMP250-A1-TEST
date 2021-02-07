59080fd0c5b7edad9075105c2f7f9dc402a334e0
Magnus Gao <64756817+MAGNUSGAO@users.noreply.github.com>
2021-02-07 14:22:54 -0500

    Update test.java
    Adding the first test, which tests whether the program outputs "-" for prices when a product's price is 0 (aka Free!)

--- assignment1/test.java
+++ assignment1/test.java
@@ -1,4 +1,27 @@
 package assignment1;
 
 public class test {
+  // Test the output of Basket toString
+    // This method tests whether the program outputs "-" for prices when a product's price is 0 (aka Free!)
+    public static void test_Basket_toString() {
+        Basket basket = new Basket();
+        basket.add(new Egg("NoEgg", 0, 4));
+        basket.add(new Fruit("Fuji Apple", 2.0, 100));
+        System.out.println(basket.toString());
+        String expected = "NoEgg\t-\n" +
+                "Fuji Apple\t2.00\n" +
+                "\n" +
+                "Subtotal\t2.00\n" +
+                "Total Tax\t-\n" +
+                "\n" +
+                "Total Cost\t2.00";
+        String actual = basket.toString().trim();
+        if (!actual.equals(expected))
+            throw new AssertionError("Incorrect format ");
+        System.out.println("Basket toString test passed. ");
+    }
+    
+    public static void main(String[] args) {
+        test_Basket_toString();
+    }
 }
