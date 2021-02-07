3877ef73652049920ec8860f59d60a9a17eb9b78
Magnus Gao <64756817+MAGNUSGAO@users.noreply.github.com>
2021-02-07 14:32:38 -0500

    Update test.java

--- assignment1/test.java
+++ assignment1/test.java
@@ -22,7 +22,30 @@ public class test {
         System.out.println("Basket toString test passed. ");
     }
     
+  // This method tests if the only item taxed in the basket is Jam (No tax on Egg and fruit)
+    public static void test_Basket_Jam_toString() {
+        Basket basket2 = new Basket();
+        basket2.add(new Egg("Large Egg", 10, 133));
+        basket2.add(new Fruit("Fuji Apple", 2.0, 100));
+        basket2.add(new Jam("Apple Jam", 10, 120));
+        String expected = "Large Egg\t1.10\n" +
+                "Fuji Apple\t2.00\n" +
+                "Apple Jam\t12.00\n" +
+                "\n" +
+                "Subtotal\t15.10\n" +
+                "Total Tax\t1.80\n" +
+                "\n" +
+                "Total Cost\t16.90";
+        String actual = basket2.toString().trim();
+        // Comment out the next line to see what your basket printed
+        // System.out.println(basket2.toString());
+        if (!actual.equals(expected))
+            throw new AssertionError("Incorrect format ");
+        System.out.println("Basket toString test passed. ");
+    }
+  
     public static void main(String[] args) {
         test_Basket_toString();
+        test_Basket_Jam_toString();
     }
 }
