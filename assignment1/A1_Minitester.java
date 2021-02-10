bafffb0ea24afbf9d42c4e704d9c0262a1718990
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2021-02-10 18:45:36 -0500

    Add tests for equals() with Fruit and SeasonalFruit

--- assignment1/A1_Minitester.java
+++ assignment1/A1_Minitester.java
@@ -2,7 +2,6 @@ package assignment1;
 
 
 import java.util.Arrays;
-import java.util.Locale;
 
 class Basket_JamTax implements Runnable{
     @Override
@@ -798,50 +797,81 @@ class Basket_Remove_Enhanced implements Runnable {
     }
 }
 
+class SeasonalFruit_Equals1 implements Runnable {
+	public void run() {
+		// Different price per weight, same final cost
+		SeasonalFruit fruit1 = new SeasonalFruit("Apples", 1, 177);
+		Fruit fruit2 = new Fruit("Apples", 1, 150);
+		
+		boolean comp = fruit2.equals(fruit1);
+		if (!comp) {
+			throw new AssertionError("Expected SeasonalFruit and Fruit with same name and cost to be equal");
+		}
+		else
+			System.out.println("SeasonalFruit_Equals1 test passed!");
+	}
+}
+
+class SeasonalFruit_Equals2 implements Runnable {
+	public void run() {
+		// Same price per weight, different final cost
+		SeasonalFruit fruit1 = new SeasonalFruit("Apples", 1, 150);
+		Fruit fruit2 = new Fruit("Apples", 1, 150);
+		
+		boolean comp = fruit2.equals(fruit1);
+		if (comp) {
+			throw new AssertionError("Expected SeasonalFruit and Fruit with different final cost to be NOT equal");
+		}
+		else 
+			System.out.println("SeasonalFruit_Equals2 test passed!");
+	}
+}
+
 public class A1_Minitester {
     // To skip running some tests, just comment them out below.
     static String[] tests = {
     		"assignment1.Basket_Remove_Enhanced",
-    	"assignment1.Basket_NullComparison",
-    	"assignment1.Basket_getProducts",
-    	"assignment1.Basket_DifferentComparison",
-        "assignment1.Basket_JamTax",
-"assignment1.Basket_NumOfProduct",
-"assignment1.Basket_Remove",
-"assignment1.Basket_add",
-"assignment1.Basket_clear",
-"assignment1.Basket_getSubTotal",
-"assignment1.Basket_toString",
-"assignment1.Customer_addFunds",
-"assignment1.Customer_addFundsException",
-"assignment1.Customer_addToBasket",
-"assignment1.Customer_checkOutBalance",
-"assignment1.Customer_checkOutException",
-"assignment1.Customer_checkOutReceipt",
-"assignment1.Customer_getBalance",
-"assignment1.Customer_getBasket",
-"assignment1.Customer_getName",
-"assignment1.Customer_removeFromBasket",
-"assignment1.Egg_Cost",
-"assignment1.Egg_Equal",
-"assignment1.Egg_Name",
-"assignment1.Fruit_Cost",
-"assignment1.Fruit_Equal",
-"assignment1.Jam_Cost",
-"assignment1.SeasonalFruit_Cost",
-"assignment1.oneEggCost",
-"assignment1.oneEggCostRoundDown",
-"assignment1.SeasonalFruit_Cost_1",
-"assignment1.fullBasket",
-"assignment1.Customer_checkOutReceipt_products_0",
-"assignment1.Basket_toString_two_decimals",
- "assignment1.fullBasket_2",
-"assignment1.test_Basket_toString",
-"assignment1.test_Basket_Jam_toString",
+	    	"assignment1.Basket_NullComparison",
+	    	"assignment1.Basket_getProducts",
+	    	"assignment1.Basket_DifferentComparison",
+	        "assignment1.Basket_JamTax",
+			"assignment1.Basket_NumOfProduct",
+			"assignment1.Basket_Remove",
+			"assignment1.Basket_add",
+			"assignment1.Basket_clear",
+			"assignment1.Basket_getSubTotal",
+			"assignment1.Basket_toString",
+			"assignment1.Customer_addFunds",
+			"assignment1.Customer_addFundsException",
+			"assignment1.Customer_addToBasket",
+			"assignment1.Customer_checkOutBalance",
+			"assignment1.Customer_checkOutException",
+			"assignment1.Customer_checkOutReceipt",
+			"assignment1.Customer_getBalance",
+			"assignment1.Customer_getBasket",
+			"assignment1.Customer_getName",
+			"assignment1.Customer_removeFromBasket",
+			"assignment1.Egg_Cost",
+			"assignment1.Egg_Equal",
+			"assignment1.Egg_Name",
+			"assignment1.Fruit_Cost",
+			"assignment1.Fruit_Equal",
+			"assignment1.Jam_Cost",
+			"assignment1.SeasonalFruit_Cost",
+			"assignment1.oneEggCost",
+			"assignment1.oneEggCostRoundDown",
+			"assignment1.SeasonalFruit_Cost_1",
+			"assignment1.fullBasket",
+			"assignment1.Customer_checkOutReceipt_products_0",
+			"assignment1.Basket_toString_two_decimals",
+ 			"assignment1.fullBasket_2",
+ 			"assignment1.test_Basket_toString",
+ 			"assignment1.test_Basket_Jam_toString",
             "assignment1.Jam_Equal1",
-"assignment1.Jam_Equal2",
-            "assignment1.Basket_Remove2"
-
+            "assignment1.Jam_Equal2",
+            "assignment1.Basket_Remove2",
+            "assignment1.SeasonalFruit_Equals1",
+            "assignment1.SeasonalFruit_Equals2"
     };
     public static void main(String[] args) {
         int numPassed = 0;
